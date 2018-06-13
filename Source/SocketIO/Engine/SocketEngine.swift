59a0707fc35ae4bcdb9d0bfdc977d7c7b7f7a2b4
dgridnev <dmitr.gridnev@gmail.com>
2018-06-13 19:49:42 +0300

    small fix for socket

--- Source/SocketIO/Engine/SocketEngine.swift
+++ Source/SocketIO/Engine/SocketEngine.swift
@@ -194,8 +194,8 @@ open class SocketEngine : NSObject, URLSessionDelegate, SocketEnginePollable, So
 
     private func handleBase64(message: String) {
         // binary in base64 string
-        let noPrefix = String(message[message.index(message.startIndex, offsetBy: 2)..<message.endIndex])
-
+        guard let noPrefix = String(message[message.index(message.startIndex, offsetBy: 2)..<message.endIndex]) else { return }
+        
         if let data = Data(base64Encoded: noPrefix, options: .ignoreUnknownCharacters) {
             client?.parseEngineBinaryData(data)
         }
